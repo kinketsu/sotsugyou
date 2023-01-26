7fb8736a56b33d1e0ff260e623ed9eb6eb000fc5
k021c6007 <k021c6007@g.neec.ac.jp>
2023-01-26 14:19:53 +0900

    0126_lee

--- calculation/js/calculation_test-1.js
+++ calculation/js/calculation_test-1.js
@@ -17,11 +17,12 @@ function inputBtn(taxRate) {
   }
 
   addPriceElement(result);
+  addTotalPrice(result);
   document.getElementById("inputPrice").value = "";
   // document.getElementById("result").innerHTML = result;
 }
 
-//結果表示
+//結果表示：priceElementの生成と削除
 function addPriceElement(price) {
   var priceView = document.getElementById("priceDataView");
   var priceElement = document.createElement("div");
@@ -29,17 +30,24 @@ function addPriceElement(price) {
   priceElement.appendChild(document.createTextNode(price));
   console.log(price);
 
+  //deleteボタン
   let deletePrice = document.createElement("button");
   deletePrice.setAttribute("id", "deleteBtn");
   deletePrice.innerText = "削除";
   deletePrice.addEventListener("click", function (event) {
     if (event.target.id == "deleteBtn") {
       let parent = this.parentElement;
+      addTotalPrice(-parseInt(parent.innerText));
       parent.remove();
+      //削除ボタンを押した時に更新処理
     }
   });
   priceElement.appendChild(deletePrice);
 }
 
-//合計結果表示
-function totalPriceView(priceElement) {}
+//合計結果表示と更新：priceElementの合計とremove処理の時値の差額処理
+function addTotalPrice(price) {
+  var totalPriceView = document.getElementById("totalPriceView");
+
+  totalPriceView.innerHTML = parseInt(totalPriceView.innerHTML) + price;
+}
